86ddf479c4101101d4fa93dfe03f6093b6daf0ca
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-06-09 15:18:53 +0700

    blok ini akan dieksekusi

--- manualExeception.cpp
+++ manualExeception.cpp
@@ -7,5 +7,11 @@ int main()
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
         throw 3,5; //melemparkan sebuah interger maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << a << " : Pengecualian akan diekesekusi" << endl;
+
+    }
     
 }
